ed09006e8d3dc1fbd60f01f39a0ce49462e6ce02
LarisaGimbu <gimbularisa@gmail.com>
2022-02-02 17:11:53 +0100

    admin index

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('guest.welcome');
-});
+})->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function(){
+        Route::get('/', 'HomeController@index')->name('index');
+    });
