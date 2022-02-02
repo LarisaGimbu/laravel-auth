e684263612d27bc9021166d34bc231082496f0a6
LarisaGimbu <gimbularisa@gmail.com>
2022-02-02 17:58:06 +0100

    elenco post

--- app/Http/Controllers/Admin/PostsController.php
+++ app/Http/Controllers/Admin/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -14,7 +15,8 @@ class PostsController extends Controller
      */
     public function index()
     {
-        return view('admin.posts.index');
+        $posts = Post::all();
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,5 +3,28 @@
 @section('content')
 <div class="container">
     <h1>Elenco post:</h1>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Title</th>
+          <th scope="col">Content</th>
+          <th scope="col">Slug</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($posts as $post)
+        <tr>
+          <th scope="row"> {{$post->id}} </th>
+          <td>{{$post->title}}</td>
+          <td>{{$post->content}}</td>
+          <td> {{$post->slug}} </td>
+        </tr>
+        @endforeach
+        
+      </tbody>
+    </table>
+
 </div>
 @endsection
