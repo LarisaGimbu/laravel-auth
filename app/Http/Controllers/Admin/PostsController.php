9bb14cedafc7199f10d67f19fff6e2a41a113aee
LarisaGimbu <gimbularisa@gmail.com>
2022-02-07 17:04:33 +0100

    paginate

--- app/Http/Controllers/Admin/PostsController.php
+++ app/Http/Controllers/Admin/PostsController.php
@@ -15,7 +15,7 @@ class PostsController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::orderBy('id', 'desc')->paginate(4);
         return view('admin.posts.index', compact('posts'));
     }
 
